14015429a34d2f7194872807583a30ff323f2c9b
Shabbar Ali <72815449+Shabbarsyed@users.noreply.github.com>
2025-01-30 14:40:21 +0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 // const corsOrigin = "http://localhost:3000"; // Default for localhost development
 
 const corsOptions = {
-  origin: ["https://bidvertiser-api.vercel.app", "http://localhost:3000", "https://random-id.ngrok.io"],
+  origin: ["https://bidvertiserdemo.vercel.app", "http://localhost:3000"],
   methods: ["GET", "POST", "DELETE", "PUT"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
